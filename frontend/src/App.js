54a9b1ce34afd9addd6c34a62ae7016d02280d42
maaz1996 <maaz.abdullah111@gmail.com>
2020-10-02 00:27:38 +0530

    project structuring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/Parts/Header";
+import Footer from "./components/Parts/Footer";
+
 function App() {
   return (
     <div>
       <Header />
-      <Footer />
     </div>
   );
 }
